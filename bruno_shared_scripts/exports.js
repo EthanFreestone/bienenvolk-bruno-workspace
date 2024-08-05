daf523e9e46d9ca580990e16f9291a71cb8e13d9
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-08-05 14:48:58 +0100

    chore: Attempts to get local scripting library set up (WIP, not currently working)

---
+++ bruno_shared_scripts/exports.js
@@ -0,0 +1,6 @@
+(() => {
+  // utils/baseUrl.js
+  var getBaseUrl = () => {
+    console.log("I'm at least working in context here");
+  };
+})();
---
+++ bruno_shared_scripts/utils/baseUrl.js
@@ -0,0 +1,3 @@
+export const getBaseUrl = () => {
+  console.log("I'm at least working in context here");
+}
